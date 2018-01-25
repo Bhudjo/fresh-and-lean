9aa0e505e16ddfb417622f28eeb12c8ce071a2b8
Alessandro Buggin <alessandrobuggin@gmail.com>
2018-01-25 22:11:35 +0000

    * fromIt2#3: refactor it2#3

--- src/main/scala/example/Cart.scala
+++ src/main/scala/example/Cart.scala
@@ -1,21 +1,22 @@
 package example
 
-case class Cart(items: List[String]) {
+case class Cart(items: Map[String,Int]) {
   val namesToPrices = Map(
     "Bananas" -> 150,
     "Apples" -> 100,
     "Cherries" -> 75)
 
   def total: Int = {
-    items.map(namesToPrices.getOrElse(_, 0)).sum
+    items.map{case (n,q) => namesToPrices.getOrElse(n, 0)*q}.sum
   }
 
   def addProduct(productName: String): Cart = {
-    Cart(items:+productName)
+    val quantity = items.getOrElse(productName, 0)+1
+    Cart(items++Map(productName->quantity))
   }
 
 }
 
 object Cart {
-  def empty = Cart(List.empty)
+  def empty = Cart(Map.empty)
 }
